ce35f0c4cabcee8afd283af20871d0f91d70b9fa
20050014 <20050014@lab02.lminf.unipmn.it>
2024-10-18 12:57:35 +0200

    Added XML

--- app/src/main/java/com/example/rockpaperscissorsapp/MainActivity.kt
+++ app/src/main/java/com/example/rockpaperscissorsapp/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.rockpaperscissorsapp
 
 import android.os.Bundle
+import android.widget.CheckBox
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    Latent var checkB: CheckBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        checkB = findViewById<CheckBox(R.id.check)
+        checkB.setOnCheckedChangeListener { compoundButton:
+                                            CompoundButton, b: Boolean ->
+            changeText(compoundButton, b)
         }
+
     }
-}
